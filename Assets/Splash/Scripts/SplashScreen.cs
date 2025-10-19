2b0d5b81563d16dbd448d50c44e844cce9e46c85
TaimoorGamez <taimoorgamez@gmail.com>
2025-10-19 19:56:59 +0500

    splash screen

--- Assets/Splash/Scripts/SplashScreen.cs
+++ Assets/Splash/Scripts/SplashScreen.cs
@@ -1,9 +1,7 @@
-using Core.Store;
 using UnityEngine;
 using DG.Tweening;
 using Core.Events;
 using Core.Plugins;
-using Core.GamePlay;
 using Core.Purchase;
 using Core.ToastMsg;
 using Core.Variables;
@@ -13,45 +11,30 @@ namespace Core.Screen
 {
     public class SplashScreen : MonoBehaviour
     {
-        //[SerializeField] ToastManager ToastMsnger;
-        //[SerializeField] Initialization FirebaseInit;
-        //[SerializeField] SOPurchase SoStore;
-        //[SerializeField] ItemData DefaultCap, DefaultFlame, DefaultSpray;
-        //[SerializeField] SOEvents InitLevelEvent;
-        //[SerializeField] DBInt LvlNum, FFT;
-        //[SerializeField] SOIntegerEvents ActiveStateEvent, DestroyStateEvent;
-        //[SerializeField] LevelManager LvlManager;
-        //[SerializeField] SOInterger MainMenuStateIndex, GamePlayStateIndex, MinLvlNum;
-        //[SerializeField] Transform FillImage;
+        [SerializeField] ToastManager ToastMsnger;
+        [SerializeField] Initialization FirebaseInit;
+        [SerializeField] SOPurchase SoStore;
+        [SerializeField] DBInt FFT;
+        [SerializeField] SOIntegerEvents ActiveStateEvent, DestroyStateEvent;
+        [SerializeField] SOInterger MainMenuStateIndex;
+        [SerializeField] Transform FillImage;
 
-        //float _loadingTime = 2;
+        float _loadingTime = 2;
 
-        //private void Start()
-        //{
-        //    if (FFT.Value != 1)
-        //    {
-        //        DefaultCap.IsPurchased = true;
-        //        DefaultFlame.IsPurchased = true;
-        //        DefaultSpray.IsPurchased = true;
-        //        FFT.Value = 1;
-        //    }
-        //    FirebaseInit.InitPlugin();
-        //    FillImage.DOScaleX(1, _loadingTime).SetEase(Ease.Linear).OnComplete(()=>
-        //    {
-        //        ToastMsnger.InitToastMsg();
-        //        if (LvlNum.Value <= MinLvlNum.Value)
-        //        {
-        //            InitLevelEvent.InvokeSOEvent();
-        //            ActiveStateEvent.InvokeSOEvent(GamePlayStateIndex.Value);
-        //        }
-        //        else
-        //        {
-        //            ActiveStateEvent.InvokeSOEvent(MainMenuStateIndex.Value);
-        //        }
-        //        DestroyStateEvent.InvokeSOEvent(0);
-        //        LvlManager.AfterEnable();
-        //        SoStore.InitializePurchasing();
-        //    });
-        //}
+        private void Start()
+        {
+            if (FFT.Value != 1)
+            {
+                FFT.Value = 1;
+            }
+            FirebaseInit.InitPlugin();
+            FillImage.DOScaleX(1, _loadingTime).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                ToastMsnger.InitToastMsg();
+                ActiveStateEvent.InvokeSOEvent(MainMenuStateIndex.Value);
+                DestroyStateEvent.InvokeSOEvent(0);
+                SoStore.InitializePurchasing();
+            });
+        }
     }
 }
